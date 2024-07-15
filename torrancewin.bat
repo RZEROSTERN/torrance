1473f857d08fbb8111601d069cbd312b6ecdcc07
Marco Ramírez <marco.ramirez@rzerocorp.com>
2024-07-15 08:17:58 -0600

    Updates

--- torrancewin.bat
+++ torrancewin.bat
@@ -1,4 +1,4 @@
-echo "All work and no play, makes Jack a dull boy" >> donotreadme.txt
+echo All work and no play, makes Jack a dull boy >> donotreadme.txt
 git stage -A
 git commit -m "Updates"
 git push origin master
